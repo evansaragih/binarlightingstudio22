5107a7da29898ceaf0a28d5545bb60b6f90d216d
“evansaragih” <evansaragih@student.unud.ac.id>
2022-09-15 00:30:47 +0800

    forth update

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('landing_page');
-// });
+Route::get('/', function () {
+    return view('landing_page');
+});
 
 Route::get('/projects', function () {
     return view('projects');
